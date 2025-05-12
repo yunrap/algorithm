dd843ec0e123078cc03709d374e3834ca435e785
yunrap <dbsdpwl7@gmail.com>
2025-05-12 11:28:52 +0900

    슬라이딩 윈도우 알고리즘

---
+++ leetcode/3_LongestSubstringWithoutRepeatingCharacters.ts
@@ -0,0 +1,18 @@
+function lengthOfLongestSubstring(s: string): number {
+  let max = 0;
+  const seen = new Map<string, number>();
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+
+    if (seen.has(currentChar)) {
+      left = Math.max(left, seen.get(currentChar)! + 1);
+    }
+    seen.set(currentChar, right);
+    max = Math.max(max, right - left + 1);
+  }
+  return max;
+}
+
+console.log(lengthOfLongestSubstring("acbabcbb"));
